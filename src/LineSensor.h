07293391875b50c3f33fa51a7922f8fe9de06d84
mtBrieger <developer@brieg.eu>
2020-07-18 11:31:32 +0200

    added lightFloor parameter, made align turning adjustable, other TODOs

--- src/LineSensor.h
+++ src/LineSensor.h
@@ -5,19 +5,23 @@
 class LineSensor {
   private:
     const uint8_t PIN_LINESENSOR = 2; // only 2 & 3 work
+    bool LIGHTFLOOR = false; // set the color of the floor, adjust the color of the tape accordingly
 
   public:
-    void begin() { pinMode(PIN_LINESENSOR, INPUT); }
+    void begin(const bool lightFloor) {
+      LIGHTFLOOR = lightFloor;
+      pinMode(PIN_LINESENSOR, INPUT);
+    }
 
     void registerListener(void (*listener)(void)) {
-        attachInterrupt(digitalPinToInterrupt(PIN_LINESENSOR), listener, FALLING);
+        attachInterrupt(digitalPinToInterrupt(PIN_LINESENSOR), listener, LIGHTFLOOR ? FALLING : RISING);
     }
 
     /**
      * Check if line is currently detected
      * */
     bool detected() {
-        bool detected = digitalRead(PIN_LINESENSOR);
+        bool detected = LIGHTFLOOR ? digitalRead(PIN_LINESENSOR) : !digitalRead(PIN_LINESENSOR);
         DEBUG_PRINT("line detected? ");
         DEBUG_PRINTLN(detected);
         return detected;
