f29600417d99e793931d1eaa94e54c5178735eb2
ModischFabrications <modisch.fabrications@gmail.com>
2020-07-20 10:19:11 +0200

    lower case light_floor

--- src/LineSensor.h
+++ src/LineSensor.h
@@ -5,23 +5,23 @@
 class LineSensor {
   private:
     const uint8_t PIN_LINESENSOR = 2; // only 2 & 3 work
-    bool LIGHTFLOOR = false; // set the color of the floor, adjust the color of the tape accordingly
+    bool light_floor = false; // set the color of the floor, adjust the color of the tape accordingly
 
   public:
     void begin(const bool lightFloor) {
-      LIGHTFLOOR = lightFloor;
+      light_floor = lightFloor;
       pinMode(PIN_LINESENSOR, INPUT);
     }
 
     void registerListener(void (*listener)(void)) {
-        attachInterrupt(digitalPinToInterrupt(PIN_LINESENSOR), listener, LIGHTFLOOR ? FALLING : RISING);
+        attachInterrupt(digitalPinToInterrupt(PIN_LINESENSOR), listener, light_floor ? FALLING : RISING);
     }
 
     /**
      * Check if line is currently detected
      * */
     bool detected() {
-        bool detected = LIGHTFLOOR ? digitalRead(PIN_LINESENSOR) : !digitalRead(PIN_LINESENSOR);
+        bool detected = light_floor ? digitalRead(PIN_LINESENSOR) : !digitalRead(PIN_LINESENSOR);
         DEBUG_PRINT("line detected? ");
         DEBUG_PRINTLN(detected);
         return detected;
